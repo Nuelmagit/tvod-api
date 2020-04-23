28a81665912be4a5718df5eaa7d87d79a530c6e2
Manuel <manuel.panizzo@univtec.com>
2020-04-23 16:25:57 -0300

    added mta y msp

--- customer.js
+++ customer.js
@@ -5,9 +5,24 @@ const sht = {
     password: 'Univtec1@'
 }
 
+const mta = {
+    api: 'mta-api.univtec.com',
+    path: 'index.php',
+    user: 'mta_portal',
+    password: 'Univtec1@'
+}
+
+const msp = {
+    api: 'msp-api.univtec.com',
+    path: 'index.php',
+    user: 'msp_portal',
+    password: 'Univtec1@'
+}
 
 const customers = {
-    sht
+    sht,
+    mta,
+    msp
 };
 
 module.exports = customers;
